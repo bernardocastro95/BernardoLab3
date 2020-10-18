d77b429c68ebb13fb6a976f2763d94fc5db39a0b
Bernardo Castro <bernardoc544@gmail.com>
2020-10-18 14:38:51 -0400

    Basic Structure

---
+++ app/src/main/java/bernardo/castro/s301073235/MainActivity.java
@@ -0,0 +1,14 @@
+package bernardo.castro.s301073235;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+//Bernardo Silveira dos Santos Castro, 301073235, Section 02
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
