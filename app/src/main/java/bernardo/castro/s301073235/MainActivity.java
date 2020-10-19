bf47aa906a3bec916b65908af8f10791dc112add
Bernardo Castro <bernardoc544@gmail.com>
2020-10-19 18:41:53 -0400

    Navigation Bar

--- app/src/main/java/bernardo/castro/s301073235/MainActivity.java
+++ app/src/main/java/bernardo/castro/s301073235/MainActivity.java
@@ -3,12 +3,19 @@ package bernardo.castro.s301073235;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 //Bernardo Silveira dos Santos Castro, 301073235, Section 02
 public class MainActivity extends AppCompatActivity {
 
+    BottomNavigationView bnv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        bnv = findViewById(R.id.bernardoNavigation);
         setContentView(R.layout.activity_main);
+
+
     }
 }
