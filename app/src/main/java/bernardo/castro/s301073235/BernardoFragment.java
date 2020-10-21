1325b7786eaf2c3c9ace8138811c752f72c103af
Bernardo Castro <bernardoc544@gmail.com>
2020-10-20 21:35:09 -0400

    Basic Fragment Structure

---
+++ app/src/main/java/bernardo/castro/s301073235/BernardoFragment.java
@@ -0,0 +1,28 @@
+package bernardo.castro.s301073235;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class BernardoFragment extends Fragment {
+
+
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_bernardo, container, false);
+    }
+}
