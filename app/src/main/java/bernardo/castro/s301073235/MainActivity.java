4f058b7ba09fbe84a2b7c56d287f4ff97039b642
Bernardo Castro <bernardoc544@gmail.com>
2020-10-22 10:58:16 -0400

    Background for fragments

--- app/src/main/java/bernardo/castro/s301073235/MainActivity.java
+++ app/src/main/java/bernardo/castro/s301073235/MainActivity.java
@@ -1,8 +1,14 @@
 package bernardo.castro.s301073235;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -16,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         bnv = findViewById(R.id.bernardoNavigation);
         setContentView(R.layout.activity_main);
 
+        }
 
-    }
+
 }
