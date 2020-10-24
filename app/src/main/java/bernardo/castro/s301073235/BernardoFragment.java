f26b009560aec14fd9c1f46a45b02fdf0d06c4cb
Bernardo Castro <bernardoc544@gmail.com>
2020-10-24 17:00:17 -0400

    Starting Draw Functionality

--- app/src/main/java/bernardo/castro/s301073235/BernardoFragment.java
+++ app/src/main/java/bernardo/castro/s301073235/BernardoFragment.java
@@ -1,28 +1,51 @@
 package bernardo.castro.s301073235;
 
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 
 public class BernardoFragment extends Fragment {
 
-
-
-
+    ImageView image;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_bernardo, container, false);
+        View v = inflater.inflate(R.layout.fragment_bernardo, container, false);
+        return v;
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        image = getView().findViewById(R.id.bernardoImage);
+        Resources r = getResources();
+        Bitmap b = BitmapFactory.decodeResource(r, R.drawable.house);
+        Canvas c = new Canvas(b.copy(Bitmap.Config.ARGB_8888, true));
+
+    }
+
 }
