4d313b164c63454e4f58e459b3a91d57b1fccb06
Bernardo Castro <bernardoc544@gmail.com>
2020-10-24 23:37:30 -0400

    Animation Functionality

--- app/src/main/java/bernardo/castro/s301073235/CastroFragment.java
+++ app/src/main/java/bernardo/castro/s301073235/CastroFragment.java
@@ -2,16 +2,22 @@ package bernardo.castro.s301073235;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.widget.Button;
+import android.widget.ImageView;
 
 
 public class CastroFragment extends Fragment {
 
-
+    Button displayAnimation;
+    Animation drawable = null;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,5 +28,19 @@ public class CastroFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_castro, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        displayAnimation = getView().findViewById(R.id.bernardoButtonAnimation);
+        displayAnimation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+    private void animation(){
+        ImageView image = getView().findViewById(R.id.benrardoAnimationFrame);
     }
 }
